c40b017450d1c47e5f7224d325d4d929f2d4bcd9
Lee Symes <leesdolphin@gmail.com>
2014-12-21 16:51:11 +1000

    Finally got audio working. Yeah.

--- main-audio-cap.py
+++ main-audio-cap.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import socket
 import struct
 import select
@@ -8,13 +9,34 @@ AUDIO_PORT = 2066
 
 sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1024 * 1024)
+net = lambda pkt, idx: ord(pkt[idx]) * 0x100 + ord(pkt[idx + 1])
 
+# Audio Details.
+# 2 Chanels
+# 48000 Sample rate
+# 32-bit big edian.
 
-print "Starting"
+# To play through VLC:
+# sudo python main-audio-cap.py | vlc --demux=rawaud --rawaud-channels 2 --rawaud-samplerate 48000 --rawaud-fourcc s32b -
+
+data_start = 0x28 + 0x02
 while 1 > 0 :
-    data = sock.recv(4096)[42:]
+    pkt = sock.recv(4096)
 
-    # header = data[0:16] # Should be 005555...5500000000
 
-    audio = data[16:]
-    sys.stdout.write(audio)
+    is_udp = ord(pkt[23]) == 17
+
+    if is_udp:
+        src_port = net(pkt,34)
+        dst_port = net(pkt,36)
+
+        if src_port == dst_port == 2066:
+            ip_header = pkt[0:data_start] # Should be 005555...5500000000
+            audio_header = pkt[data_start:data_start + 16]
+
+            # print([hex(ord(c)) for c in ip_header]
+            # print [hex(ord(c)) for c in audio_header]
+            print(len(pkt) - (data_start + 16), file=sys.stderr)
+
+            audio = pkt[data_start + 16:]
+            sys.stdout.write(audio)
