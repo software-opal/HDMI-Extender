a481e731bf8f6e2524f8203d7b3c3cf7ebd1994e
Lee Symes <leesdolphin@gmail.com>
2014-12-21 17:37:29 +1000

    Started cleaning up video capture to allow saving & re-streaming of the video.

--- main-video-cap.py
+++ main-video-cap.py
@@ -9,11 +9,68 @@ import os
 import cv2
 import numpy as np
 
+MAGIC_PACKET_NO = 0x8000
+
+
+
+def handle_partial_packet(pkts, f_no, p_no):
+    return # I don't care about dropping packets.
+    # But this is the code I might use if I did care.
+    frame = "".join(pkts)
+    if p_no >= MAGIC_PACKET_NO:
+        f = open("partial-frames/{:d} - last - (revd {:d}) > (reqd {:d}).jpeg".format(f_no, len(pkts), last_pkt_no), "w")
+    else:
+        f = open("partial-frames/{:d} - first - (revd {:d}) > (reqd {:d}).jpeg".format(f_no, len(pkts), last_pkt_no), "w")
+    f.write(frame)
+    f.close()
+
+def handle_full_packet(pkts, curr_frame, curr_pkt_no):
+    frame = "".join(pkts)
+    display_frame(frame)
+    # TODO save to file and produce stream
+    # save_frame(frame)
+    # transmit_frame(frame)
+
+if "-d" in sys.argv:
+    def display_frame(frame):
+        try:
+            i = cv2.imdecode(np.fromstring(frame, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
+            cv2.imshow('i',i)
+            k = cv2.waitKey(1)
+            if k == 27:
+                exit(0)
+            if k == 32:
+                f = open("f-{:d}.jpeg".format(f_no), "w")
+                f.write(frame)
+                f.close()
+        except:
+            pass
+else:
+    display_frame = lambda frame: None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1024 * 1024)
-
-# print("video/mjpeg")
 
 
 net = lambda pkt, idx: ord(pkt[idx]) * 0x100 + ord(pkt[idx + 1])
@@ -40,11 +97,7 @@ while 1 > 0 :
 
             if p_no == 0:
                 if len(pkts) > 0:
-                    frame = "".join(pkts)
-                    f = open("partial-frames/{:d} - first - (revd {:d}) > (reqd {:d}).jpeg".format(f_no, len(pkts), last_pkt_no), "w")
-                    f.write(frame)
-                    f.close()
-
+                    handle_partial_packet(pkt, f_no, p_no)
                 pkts = []
 
             data = pkt[46:]
@@ -53,32 +106,9 @@ while 1 > 0 :
             if p_no > 32768:
                 last_pkt_no = p_no - 32768
                 if len(pkts) != last_pkt_no + 1:
-                    print(len(pkts), last_pkt_no)
-                    print("FAIL!")
-                    frame = "".join(pkts)
-                    f = open("partial-frames/{:d} - last - (revd {:d}) > (reqd {:d}).jpeg".format(f_no, len(pkts), last_pkt_no), "w")
-                    f.write(frame)
-                    f.close()
-                    # print(pkts)
+                    handle_partial_packet(pkts, f_no, p_no)
                 else:
-                    frame = "".join(pkts)
-                    # TODO save to file and produce stream
-                    try:
-                        pass
-                        i = cv2.imdecode(np.fromstring(frame, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
-                        cv2.imshow('i',i)
-                        k = cv2.waitKey(1)
-                        if k == 27:
-                            exit(0)
-                        if k == 32:
-                            f = open("f-{:d}.jpeg".format(f_no), "w")
-                            f.write(frame)
-                            f.close()
-                    except:
-                        print("Frame: {:d}".format(f_no), file=sys.stderr)
-                        f = open("frames/f-{:d}.jpeg".format(f_no), "w")
-                        f.write(frame)
-                        f.close()
+                    handle_full_packet(pkts, f_no, p_no)
                 pkts = []
 
 
